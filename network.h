c60cda3fe970c5d8025efa4d5a137796dd59b3e3
vjt <vjt@antifork.org>
2002-12-27 04:53:41 +0000

    prettyfied protos

--- network.h
+++ network.h
@@ -1,11 +1,16 @@
 #ifndef __network_h__
 #define __network_h__
 
-GIOChannel *connect_server(gchar *, unsigned gint);
-gboolean net_send_callback(GIOChannel *, GIOCondition);
-gboolean net_receive_callback(GIOChannel *, GIOCondition);
-gboolean net_err_callback(GIOChannel *, GIOCondition);
-int net_transmit(GIOChannel *, const gchar *, gint);
+GIOChannel *connect_server(gchar *, guint);
+
+gboolean net_send_callback(GIOChannel *);
+gboolean net_receive_callback(GIOChannel *);
+gboolean net_err_callback(GIOChannel *);
+
+gint net_transmit(GIOChannel *, const gchar *, gint);
+gchar *net_receive(GIOChannel *, gsize *);
+gboolean net_flush(GIOChannel *);
+
 void send_out(gchar *, ...);
 
 #endif
