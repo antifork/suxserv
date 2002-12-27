72df14963119ac399c25fca3779788789713eafc
vjt <vjt@antifork.org>
2002-12-27 05:42:38 +0000

    added some functionality

--- tools/netxplode-persist.sh
+++ tools/netxplode-persist.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
+test -z "$1" &&
+{
+    echo "usage: $0 <timeout>";
+    exit -1;
+}
+
+test "$1" -lt 60 &&
+{
+    echo "timeouts smaller than 60 secs are NOT recommended !!";
+    exit 1;
+}
+
+timeout=$1;
+
+trap "killall netxplode.pl ; exit" 2 3 15
+
 while true; do
-	./netxplode.pl &
-	sleep 600;
+	./netxplode.pl >/dev/null 2>&1 &
+	sleep $timeout;
 done
