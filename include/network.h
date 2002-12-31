db3318f1abcad79c2781418f9becbb6c14225b7c
vjt <vjt@antifork.org>
2002-12-31 01:28:52 +0000

    new net_shutdown() function

--- include/network.h
+++ include/network.h
@@ -10,6 +10,7 @@ gboolean net_err_callback(GIOChannel *);
 gint net_transmit(GIOChannel *, const gchar *, gint);
 gchar *net_receive(GIOChannel *, gsize *);
 gboolean net_flush(GIOChannel *);
+gboolean net_shutdown(GIOChannel *);
 
 void send_out(gchar *, ...);
 
