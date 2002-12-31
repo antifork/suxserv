805a2c619fd67f6ca2a05a3b0fb8b4556d3819c2
vjt <vjt@antifork.org>
2002-12-31 22:35:09 +0000

    added table dealloc helper func

--- src/table.c
+++ src/table.c
@@ -1,4 +1,5 @@
 #include "sux.h"
+#define TABLE_C
 #include "table.h"
 #include "main.h"
 #include "match.h"
@@ -23,6 +24,11 @@ G_INLINE_FUNC guint FNV_hash(guchar *s)
 	h = ((h * FNV_PRIME) ^ *e--);
 
     return h;
+}
+
+static void hash_tbl_dealloc(gchar *key, gpointer *data, GMemChunk *mem_pool)
+{
+    g_mem_chunk_free(mem_pool, data);
 }
 
 TABLE_DECLARE(user, User, FNV_hash, nick, gchar);
