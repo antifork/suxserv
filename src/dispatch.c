59177ed9e199bfbd773f5f5ae9ea53b54176b0f2
vjt <vjt@antifork.org>
2002-12-31 05:21:31 +0000

    trying to complete channel support

--- src/dispatch.c
+++ src/dispatch.c
@@ -12,6 +12,7 @@ REMOTE_TABLE_INSTANCE(user);
 REMOTE_TABLE_INSTANCE(channel);
 
 REMOTE_MEMPOOL_INSTANCE(cmembers);
+REMOTE_MEMPOOL_INSTANCE(links);
 
 gint m_private(gint parc, gchar **parv)
 {
@@ -57,10 +58,6 @@ gint m_topic(gint parc, gchar **parv)
     return 1;
 }
 gint m_join(gint parc, gchar **parv)
-{
-    DUMMY
-}
-gint m_part(gint parc, gchar **parv)
 {
     DUMMY
 }
@@ -162,13 +159,82 @@ gint m_notice(gint parc, gchar **parv)
 gint m_quit(gint parc, gchar **parv)
 {
     User *u = _TBL(user).get(parv[0]);
+    Channel *c;
+    GSList *johnny; /* the walker */
 
     g_return_val_if_fail(u != NULL, 0);
+
+    for(johnny = u->channels; johnny; johnny = g_slist_next(u->channels))
+    {
+	GSList *jimmy; /* the second walker */
+	c = _TBL(channel).get(((SLink *)johnny->data)->value.c->chname);
+		
+	/* XXX wtf does this not work ?? */
+	for(jimmy = c->members; jimmy; jimmy = g_slist_next(c->members))
+	{
+	    if(((ChanMember*)jimmy->data)->u == u)
+	    {
+		/* remove this link */
+		c->members = g_slist_remove_link(c->members, jimmy);
+		g_mem_chunk_free(_MPL(cmembers), jimmy->data);
+		g_slist_free_1(jimmy);
+
+		break;
+	    }
+	}
+    }
+
+    for(johnny = u->channels; johnny; johnny = g_slist_next(u->channels))
+    {
+	g_mem_chunk_free(_MPL(links), johnny->data);
+    }
+
+    g_slist_free(u->channels);
     
     _TBL(user).del(u);
     _TBL(user).destroy(u);
     return 1;
 }
+
+/*
+ * m_part 
+ * parv[0] = sender prefix 
+ * parv[1] = channel
+ * parv[2] = Optional part reason
+ */
+gint m_part(gint parc, gchar **parv)
+{
+    gchar *pfx = parv[0];
+    gchar *chname = parv[1];
+
+    Channel *c;
+    User *u;
+    GSList *johnny; /* the walker */
+    
+    g_return_val_if_fail(pfx != NULL, 0);
+    g_return_val_if_fail(chname != NULL, 0);
+
+    c = _TBL(channel).get(chname);
+    g_return_val_if_fail(c != NULL, 0);
+
+    u = _TBL(user).get(pfx);
+    g_return_val_if_fail(u != NULL, 0);
+    
+    for(johnny = c->members; johnny; johnny = g_slist_next(c->members))
+    {
+	if(((ChanMember*)johnny->data)->u == u)
+	{
+	    c->members = g_slist_remove_link(c->members, johnny);
+	    g_mem_chunk_free(_MPL(cmembers), johnny->data);
+	    g_slist_free_1(johnny);
+	    
+	    break;
+	}
+    }
+
+    return 1;
+}
+
 gint m_kill(gint parc, gchar **parv)
 {
     DUMMY
@@ -196,10 +262,27 @@ gint m_motd(gint parc, gchar **parv)
     send_out(rpl_str(RPL_ENDOFMOTD), me.name, parv[0]);
     return 0;
 }
+
+/*
+ *  m_server 
+ *       parv[0] = sender prefix 
+ *       parv[1] = servername 
+ *       parv[2] = serverinfo/hopcount 
+ *       parv[3] = serverinfo
+ */
 gint m_server(gint parc, gchar **parv)
 {
-    DUMMY
+    gint hopcount;
+    
+    strcpy(me.uplink, parv[1]);
+    hopcount = atoi(parv[2]);
+
+    g_message("Linked with %s [%s], distant %d hop%s",
+	    me.uplink, parv[3], hopcount, hopcount != 1 ? "s" : "");
+
+    return 0;
 }
+
 gint m_info(gint parc, gchar **parv)
 {
     gint i;
@@ -265,9 +348,6 @@ gint m_svinfo(gint parc, gchar **parv)
     theirtime = atol(parv[4]);
     deltat = abs(theirtime - tmptime);
 
-    g_message("Linked with %s, TS protocol version: %s",
-	    parv[0], parv[1]);
-
     if(deltat > 45)
     {
 	g_warning("Link %s dropped, excessive TS delta (%ld)", parv[0], deltat);
@@ -284,9 +364,14 @@ gint m_svinfo(gint parc, gchar **parv)
     return 0;
 }
 
-static gint cm_compare(ChanMember *cm, gchar *s)
+G_INLINE_FUNC gint cm_compare(ChanMember *cm, gchar *s)
 {
     return mycmp(s, cm->u->nick);
+}
+
+G_INLINE_FUNC gint ch_compare(SLink *lp, gchar *s)
+{
+    return mycmp(s, lp->value.c->chname);
 }
 
 static gboolean compile_mode(Mode *m, gchar *s, gint parc, gchar **parv)
@@ -350,6 +435,27 @@ static gboolean compile_mode(Mode *m, gchar *s, gint parc, gchar **parv)
     return TRUE;
 }
 
+static void add_user_to_channel(User *u, Channel *c, guint flags)
+{
+    ChanMember *cm = g_mem_chunk_alloc0(_MPL(cmembers));
+
+    g_return_if_fail(c != NULL);
+    g_return_if_fail(u != NULL);
+    g_return_if_fail(cm != NULL);
+    
+    cm->u = u;
+    cm->flags = flags;
+
+    if(!g_slist_find_custom(u->channels, c->chname, (GCompareFunc) ch_compare))
+    {
+	SLink *lp = g_mem_chunk_alloc0(_MPL(links));
+	lp->value.c = c;
+	u->channels = g_slist_prepend(u->channels, lp);
+    }	
+    
+    c->members = g_slist_prepend(c->members, cm);
+}
+
 /*
  * m_sjoin 
  * parv[0] - sender 
@@ -369,24 +475,38 @@ gint m_sjoin(gint parc, gchar **parv)
     Channel *c = NULL;
     Mode mode;
     gint i;
-    
-    if(parc < 5)
-    {
-	g_warning("received SJOIN with less than 5 params (%d)", parc);
-	return 0;
-    }
-    
+
+    g_return_val_if_fail(parv != NULL, 0);
+
     ts = atoi(parv[1]);
     channel = parv[2];
-    modes = parv[3];
-    users = parv[4];
 
     if(!(c = _TBL(channel).get(channel)))
     {
 	c = _TBL(channel).alloc(channel);
+    }
+    g_return_val_if_fail(c != NULL, 0);
+
+    if(parc == 3)
+    {
+	/* client sjoin with timestamp */
+	s = parv[0];
+	c->ts = ts;
+
+	add_user_to_channel(_TBL(user).get(s), c, 0);
+
+	return 0;
+    }
+    else
+    {
+	/* server sjoin with modes and users */
+
+	modes = parv[3];
+	users = parv[4];
+	
 	c->ts = ts;
 	c->bans = NULL;
-	c->users = NULL;
+	c->members = NULL;
 	if(compile_mode(&mode, modes, parc, parv) == FALSE)
 	{
 	    g_warning("failed mode compilation on %s", modes);
@@ -394,10 +514,6 @@ gint m_sjoin(gint parc, gchar **parv)
 	    return 0;
 	}
 	c->mode = mode;
-    }
-    else if(c->ts != ts)
-    {
-	c->ts = ts;
     }
 
     users_arr = g_strsplit(users, " ", 0);
@@ -419,13 +535,9 @@ gint m_sjoin(gint parc, gchar **parv)
 	while(*s == '@' || *s == '+')
 	    s++;
 	
-	if(!g_slist_find_custom(c->users, s, (GCompareFunc) cm_compare))
+	if(!g_slist_find_custom(c->members, s, (GCompareFunc) cm_compare))
 	{
-	    ChanMember *cm = g_mem_chunk_alloc0(_MPL(cmembers));
-	    cm->u = _TBL(user).get(s);
-	    cm->flags = fl;
-	    
-	    c->users = g_slist_prepend(c->users, cm);
+	    add_user_to_channel(_TBL(user).get(s), c, fl);
 	}
     }
     
