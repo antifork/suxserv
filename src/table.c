828e191be78501ae3d8e849bf961687c6093924c
vjt <vjt@antifork.org>
2003-01-03 09:05:15 +0000

    added (disabled) hash functions from ircd

--- src/table.c
+++ src/table.c
@@ -18,10 +18,84 @@ G_INLINE_FUNC guint FNV_hash(guchar *s)
     guchar *e = s + strlen(s) - 1;
 
     while(e != s)
-	h = ((h * FNV_PRIME) ^ *e--);
+	h = ((h * FNV_PRIME) ^ tolower(*e--));
 
     return h;
 }
+
+#if 0
+
+/* Taner had BITS_PER_COL 4 BITS_PER_COL_MASK 0xF - Dianora */
+
+#define BITS_PER_COL 3
+#define BITS_PER_COL_MASK 0x7
+#define MAX_SUB     (1<<BITS_PER_COL)
+
+/* Client hash table 
+ * used in hash.c 
+ */
+
+#define U_MAX_INITIAL  8192
+#define U_MAX_INITIAL_MASK (U_MAX_INITIAL-1)
+#define U_MAX (U_MAX_INITIAL*MAX_SUB)
+
+/* Channel hash table 
+ * used in hash.c 
+ */
+
+#define CH_MAX_INITIAL  2048
+#define CH_MAX_INITIAL_MASK (CH_MAX_INITIAL-1)
+#define CH_MAX (CH_MAX_INITIAL*MAX_SUB)
+
+/*
+ * stolen from bahamut/src/hash.c
+ */
+guint hash_nick_name(gchar *nname)
+{
+    register guint hash = 0;
+    register gint hash2 = 0;
+    register gchar lower;
+
+    while (*nname)
+    {
+	lower = tolower(*nname);
+	hash = (hash << 1) + lower;
+	hash2 = (hash2 >> 1) + lower;
+	nname++;
+    }
+
+    return ((hash & U_MAX_INITIAL_MASK) << BITS_PER_COL) +
+	(hash2 & BITS_PER_COL_MASK);
+}
+
+/*
+ * hash_channel_name
+ * 
+ * calculate a hash value on at most the first 30 characters of the
+ * channel name. Most names are short than this or dissimilar in this
+ * range. There is little or no point hashing on a full channel name
+ * which maybe 255 chars long.
+ */
+gint hash_channel_name(guchar *hname)
+{
+    register guint hash = 0;
+    register gint hash2 = 0;
+    register gint i = 30;
+    register gchar lower;
+
+    while (*hname && --i)
+    {
+	lower = tolower(*hname);
+	hash = (hash << 1) + lower;
+	hash2 = (hash2 >> 1) + lower;
+	hname++;
+    }
+
+    return ((hash & CH_MAX_INITIAL_MASK) << BITS_PER_COL) +
+	(hash2 & BITS_PER_COL_MASK);
+}
+
+#endif /* 0 */
 
 G_INLINE_FUNC void hash_tbl_dealloc(gchar *key, gpointer *data, GMemChunk *mem_pool)
 {
