d108ee87145c931eadc25fecaf766674e04be510
vjt <vjt@antifork.org>
2003-01-03 09:09:05 +0000

    removed function calls

--- include/network.h
+++ include/network.h
@@ -7,10 +7,9 @@ gboolean net_send_callback(GIOChannel *);
 gboolean net_receive_callback(GIOChannel *);
 gboolean net_err_callback(GIOChannel *);
 
-gint net_transmit(GIOChannel *, const gchar *, gint);
-gchar *net_receive(GIOChannel *, gsize *);
-gboolean net_flush(GIOChannel *);
 gboolean net_shutdown(GIOChannel *);
+
+void setup_netbuf(void);
 
 void send_out(gchar *, ...) G_GNUC_PRINTF(1, 2);
 
