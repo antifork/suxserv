c31b84674f25d23929cc64702c7cb0631cda100f
vjt <vjt@antifork.org>
2003-01-07 10:10:09 +0000

    new autoconf/automake/etc related files

---
+++ autogen.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+test -f configure.in || {
+    echo "**Error**: This directory does not look like the top-level directory"
+    exit 1
+}
+
+#exit on failure 
+set -e
+
+echo running aclocal ...
+aclocal
+
+echo running autoheader ...
+autoheader 
+
+echo running automake ...
+automake -a --copy 
+
+echo running autoconf ...
+autoconf
+
+echo running libtoolize ...
+libtoolize --automake -c
+
+echo all done !
